7349313f8e253831e0d693beaf4c499c6b0bbe43
magda <magda17095@wp.pl>
2020-01-06 13:01:49 +0100

    atoms done

--- note-app/.storybook/config.js
+++ note-app/.storybook/config.js
@@ -1,4 +1,13 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../src/theme/mainTheme';
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/components', true, /\.stories\.js$/), module);
+function loadStories() {
+  const req = require.context('../src/components', true, /\.stories\.js$/);
+  req.keys().forEach(filename => req(filename));
+}
+
+addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
+
+configure(loadStories, module);
